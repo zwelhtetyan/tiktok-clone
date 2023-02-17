0c64736824e1bcf0a4bf59ad5c01d77a3539b51f
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-02-18 00:34:24 +0630

    PWA 🚀🚀🚀

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
-};
+});
 
 module.exports = nextConfig;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='apple-touch-icon' href='/icon-192x192.png' />
+        <meta name='theme-color' content='#121212' />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }

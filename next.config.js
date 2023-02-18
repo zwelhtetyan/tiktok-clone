cf248e7ea4ae5dc9fea3323123ff36674b7c9f85
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-02-18 19:16:07 +0630

    add runtimeCaching

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withPWA = require('next-pwa')({
   skipWaiting: true,
   cacheStartUrl: false,
   dynamicStartUrl: true,
+  runtimeCaching: [],
   disable: process.env.NODE_ENV === 'development',
 });
 

2afd358783d142a4ce888d535b062810478dc904
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-07-21 17:00:10 +0630

    style: change suggested account container width

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -1,12 +1,12 @@
-import axios from 'axios';
-import useSWR from 'swr';
-import { ROOT_URL } from '../utils';
-import { User } from '../types';
-import UserProfile from './UserProfile';
-import { generateFakeUsername } from '../utils/generateFakeUsername';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import useStore from '../store';
+import axios from "axios";
+import useSWR from "swr";
+import { ROOT_URL } from "../utils";
+import { User } from "../types";
+import UserProfile from "./UserProfile";
+import { generateFakeUsername } from "../utils/generateFakeUsername";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import useStore from "../store";
 
 interface DataProps {
   data: User[];
@@ -31,13 +31,13 @@ function UserAccount({
   return (
     <Link
       href={`/profile/${userId}`}
-      className='flex items-center dark:text-gray-200 rounded p-[2px] hover:bg-gray-100 dark:hover:bg-darkBtnHover cursor-pointer'
+      className="flex items-center dark:text-gray-200 rounded p-[2px] hover:bg-gray-100 dark:hover:bg-darkBtnHover cursor-pointer"
     >
-      <UserProfile src={src} className=' mr-2' />
+      <UserProfile src={src} className=" mr-2" />
 
       <div>
-        <h2 className='font-semibold leading-5'>{userName}</h2>
-        <p className='text-gray-500 text-sm'>
+        <h2 className="font-semibold leading-5">{userName}</h2>
+        <p className="text-gray-500 text-sm">
           {generateFakeUsername(userName)}
         </p>
       </div>
@@ -47,13 +47,13 @@ function UserAccount({
 
 function UserSkeleton() {
   return (
-    <div className='shadow rounded-md p-[2px] w-full mb-1'>
-      <div className='animate-pulse flex items-center space-x-2'>
-        <div className='rounded-full bg-gray-300 dark:bg-darkBtn h-10 w-10' />
+    <div className="shadow rounded-md p-[2px] w-full mb-1">
+      <div className="animate-pulse flex items-center space-x-2">
+        <div className="rounded-full bg-gray-300 dark:bg-darkBtn h-10 w-10" />
 
-        <div className='flex-1 space-y-2 py-1'>
-          <div className='h-2 bg-gray-300 dark:bg-darkBtn rounded w-2/3'></div>
-          <div className='h-2 bg-gray-300 dark:bg-darkBtn rounded w-36'></div>
+        <div className="flex-1 space-y-2 py-1">
+          <div className="h-2 bg-gray-300 dark:bg-darkBtn rounded w-2/3"></div>
+          <div className="h-2 bg-gray-300 dark:bg-darkBtn rounded w-36"></div>
         </div>
       </div>
     </div>
@@ -67,7 +67,7 @@ export default function SuggestedAccounts() {
   const { suggestedUsers, setSuggestedUsers } = useStore();
 
   const { data: allUsers, isLoading }: DataProps = useSWR(
-    'getAllUsers',
+    "getAllUsers",
     getAllUsers
   );
 
@@ -82,10 +82,10 @@ export default function SuggestedAccounts() {
   }, [allUsers, setSuggestedUsers]);
 
   return (
-    <div className='pt-4 mt-4 hidden lg:block border-t border-t-gray-200 dark:border-t-darkBorder'>
-      <h2 className='font-semibold text-gray-500'>Suggested accounts</h2>
+    <div className="pt-4 mt-4 hidden lg:block border-t border-t-gray-200 dark:border-t-darkBorder">
+      <h2 className="font-semibold text-gray-500">Suggested accounts</h2>
 
-      <div className='flex-1 max-h-[270px] mt-3 overflow-y-auto'>
+      <div className="flex-1 max-h-[300px] mt-3 overflow-y-auto">
         {isLoading &&
           [1, 2, 3, 4, 5].map((_, idx) => <UserSkeleton key={idx} />)}
 
